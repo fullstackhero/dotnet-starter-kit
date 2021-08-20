378927380e9e024917826765241796fad3fb67c1
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-20 18:35:45 +0530

    structure cleanup

--- src/Bootstrapper/Startup.cs
+++ src/Bootstrapper/Startup.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Bootstrapper
