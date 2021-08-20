275f1e1efbd123796f03d7b36513067da5eaf3fa
Mukesh Murugan <iammukeshm@gmail.com>
2021-08-20 19:05:42 +0530

    Guid

--- src/Core/Domain/Contracts/BaseEntity.cs
+++ src/Core/Domain/Contracts/BaseEntity.cs
@@ -2,6 +2,11 @@ namespace DN.WebApi.Domain.Contracts
 {
     public abstract class BaseEntity
     {
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
     }
 }
