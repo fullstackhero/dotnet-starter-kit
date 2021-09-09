a8b633f62e558c88d266afb941a6581065313c79
Mukesh Murugan <iammukeshm@gmail.com>
2021-09-09 23:56:42 +0530

    exception update

--- src/Core/Application/Exceptions/IdentityException.cs
+++ src/Core/Application/Exceptions/IdentityException.cs
@@ -4,7 +4,7 @@ namespace DN.WebApi.Application.Exceptions
 {
     public class IdentityException : CustomException
     {
-        public IdentityException(string message, List<string> errors = default, HttpStatusCode statusCode = default)
+        public IdentityException(string message, List<string> errors = default, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
             : base(message, errors, statusCode)
         {
         }
