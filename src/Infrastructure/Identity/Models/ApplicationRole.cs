38f3d1106877b0cf5b116eeda93eac2629f2eb16
Mukesh Murugan <iammukeshm@gmail.com>
2021-09-10 04:53:06 +0530

    role tenancy fix

--- src/Infrastructure/Identity/Models/ApplicationRole.cs
+++ src/Infrastructure/Identity/Models/ApplicationRole.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace DN.WebApi.Infrastructure.Identity.Models
 {
-    public class ApplicationRole : IdentityRole
+    public class ApplicationRole : IdentityRole, IIdentityTenant
     {
         public string Description { get; set; }
         public string TenantId { get; set; }
