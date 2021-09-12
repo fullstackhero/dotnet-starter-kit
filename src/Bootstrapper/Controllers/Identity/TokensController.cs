ff29d366469eadbacf95b48118b0407a1e4ae0a4
akema-trebla <joshmcgrath7@gmail.com>
2021-09-12 17:56:56 +0000

    Add Swagger Filter To TokensController

--- src/Bootstrapper/Controllers/Identity/TokensController.cs
+++ src/Bootstrapper/Controllers/Identity/TokensController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using DN.WebApi.Application.Abstractions.Services.Identity;
 using DN.WebApi.Infrastructure.SwaggerFilters;
 using DN.WebApi.Shared.DTOs.Identity.Requests;
--- src/Infrastructure/SwaggerFilters/AddTenantIdFilter.cs
+++ src/Infrastructure/SwaggerFilters/AddTenantIdFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.OpenApi.Any;
+﻿using System.Reflection;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
