6f51fdfa9bc92de2ffbf9e2157e6bc1cc9f267d1
Ittyab Rehman <ittyaboffical@gmail.com>
2021-09-21 22:44:27 +0500

    mapsterSettings.Configure() details added

--- src/Shared/Shared.DTOs/MapsterSettings.cs
+++ src/Shared/Shared.DTOs/MapsterSettings.cs
@@ -4,6 +4,14 @@
     {
         public static void Configure()
         {
+            // here we will define the type conversion / Custom-mapping
+            // More details at https://github.com/MapsterMapper/Mapster/wiki/Custom-mapping
+
+            // for example
+            /*TypeAdapterConfig<TSource, TDestination>
+                .NewConfig()
+                .Map(dest => dest.Gender,      //Genders.Male or Genders.Female
+                    src => src.GenderString); //"Male" or "Female"*/
 
         }
     }
