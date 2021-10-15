77732d7a64960fab1695ab18e33c2c9fa8c3b93b
Mukesh Murugan <iammukeshm@gmail.com>
2021-10-15 07:50:21 +0530

    added newId #95

--- src/Core/Domain/Contracts/BaseEntity.cs
+++ src/Core/Domain/Contracts/BaseEntity.cs
@@ -1,4 +1,5 @@
 using System;
+using MassTransit;
 
 namespace DN.WebApi.Domain.Contracts
 {
@@ -8,36 +9,7 @@ namespace DN.WebApi.Domain.Contracts
 
         protected BaseEntity()
         {
-            Id = GenerateComb();
+            Id = NewId.Next().ToGuid();
         }
-
-        private static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
-
-        private Guid GenerateComb()
-        {
-            byte[] guidArray = Guid.NewGuid().ToByteArray();
-
-            DateTime now = DateTime.UtcNow;
-
-            // Get the days and milliseconds which will be used to build the byte string
-            TimeSpan days = new TimeSpan(now.Ticks - BaseDateTicks);
-            TimeSpan msecs = now.TimeOfDay;
-
-            // Convert to a byte array
-            // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
-            byte[] daysArray = BitConverter.GetBytes(days.Days);
-            byte[] msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds / 3.333333));
-
-            // Reverse the bytes to match SQL Servers ordering
-            Array.Reverse(daysArray);
-            Array.Reverse(msecsArray);
-
-            // Copy the bytes into the guid
-            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
-
-            return new Guid(guidArray);
-        }
-
     }
 }
