18ffe4581828ae1041206a19d47c35283d599148
justSteve <steve@juststeve.com>
2021-10-21 13:42:26 -0500

    Adds Serilog sinks for Seq and SQL

--- src/Bootstrapper/Startup.cs
+++ src/Bootstrapper/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace DN.WebApi.Bootstrapper
 {
@@ -42,6 +43,8 @@ namespace DN.WebApi.Bootstrapper
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            app.UseSerilogRequestLogging();
 
             app.UseInfrastructure(_config);
         }
