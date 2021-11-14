212896fd3266bce4330e21d3735400435cdb91c7
Mukesh Murugan <iammukeshm@gmail.com>
2021-11-14 12:39:06 +0530

    cleanup

--- src/Infrastructure/Middlewares/RequestLoggingMiddleware.cs
+++ src/Infrastructure/Middlewares/RequestLoggingMiddleware.cs
@@ -25,7 +25,6 @@ namespace DN.WebApi.Infrastructure.Middlewares
 
         public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
         {
-
             LogContext.PushProperty("RequestTimeUTC", DateTime.UtcNow);
             string requestBody = string.Empty;
             if (httpContext.Request.Path.ToString().Contains("tokens"))
@@ -46,7 +45,6 @@ namespace DN.WebApi.Infrastructure.Middlewares
                     // rewind for next middleware.
                     request.Body.Position = 0;
                 }
-
             }
 
             LogContext.PushProperty("RequestBody", requestBody);
