8ade336b2f4c1bf709b0d5e3f2e0bdb316566059
FrankyJQuintero <frankyjquintero@gmail.com>
2021-11-21 03:15:52 -0500

    Add BasicNotification for tooltip

---
+++ src/Shared/Shared.DTOs/Notifications/BasicNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace DN.WebApi.Shared.DTOs.Notifications
+{
+    public class BasicNotification : INotificationMessage
+    {
+        public string TypeMessage { get; set; } = typeof(BasicNotification).Name;
+        public string Message { get; set; }
+    }
+}
