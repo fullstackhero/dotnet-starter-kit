5e1b16a429d0d53f7a47ccc5c4c5051c15c92619
xlogex <xlogex@hotmail.com>
2021-11-29 12:14:19 +0100

    formatting

--- src/Infrastructure/Common/Extensions/CORSExtensions.cs
+++ src/Infrastructure/Common/Extensions/CORSExtensions.cs
@@ -6,18 +6,20 @@ namespace DN.WebApi.Infrastructure.Common.Extensions;
 
 public static class CorsExtensions
 {
-    private static string[] GetWithOrigins(CorsSettings corsSettings){
-        var result = new string[0]
-        .Union(corsSettings.Angular.Split(';').Where(o1 => string.IsNullOrWhiteSpace(o1) == false))
-        .Union(corsSettings.Blazor.Split(';').Where(o2 => string.IsNullOrWhiteSpace(o2) == false))
-        .ToArray();
+    private static string[] GetWithOrigins(CorsSettings corsSettings)
+    {
+        string[] result = new string[0]
+            .Union(corsSettings.Angular.Split(';').Where(o1 => string.IsNullOrWhiteSpace(o1) == false))
+            .Union(corsSettings.Blazor.Split(';').Where(o2 => string.IsNullOrWhiteSpace(o2) == false))
+            .ToArray();
         return result;
     }
+
     internal static IServiceCollection AddCorsPolicy(this IServiceCollection services)
     {
         var corsSettings = services.GetOptions<CorsSettings>(nameof(CorsSettings));
 
-        var withOrigins = GetWithOrigins(corsSettings);
+        string[] withOrigins = GetWithOrigins(corsSettings);
 
         return services.AddCors(opt =>
         {
