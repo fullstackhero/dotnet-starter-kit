c43f6da53eb8e931db1d3e3759a620ef2d511464
EXT2011862 <g.lanzetta@reply.it>
2021-11-29 11:04:00 +0100

    to use multiple endpoints for each client type

--- src/Infrastructure/Common/Extensions/CORSExtensions.cs
+++ src/Infrastructure/Common/Extensions/CORSExtensions.cs
@@ -9,11 +9,17 @@ public static class CorsExtensions
     internal static IServiceCollection AddCorsPolicy(this IServiceCollection services)
     {
         var corsSettings = services.GetOptions<CorsSettings>(nameof(CorsSettings));
+
+        var withOrigins = new string[0]
+        .Union(corsSettings.Angular.Split(';').Where(o1 => string.IsNullOrWhiteSpace(o1) == false))
+        .Union(corsSettings.Blazor.Split(';').Where(o2 => string.IsNullOrWhiteSpace(o2) == false))
+        .ToArray();
+
         return services.AddCors(opt =>
         {
             opt.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(new string[] { corsSettings.Angular, corsSettings.Blazor });
+                policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(withOrigins);
             });
         });
     }
