2763e925637f607bb151f32d6a01b2205c8d1082
fretje <github@fretje.be>
2021-11-30 21:26:06 +0100

    fix some errors with swaggersettings
    also fix spelling mistake ;)
    
    also fix bug with CorsExtensions.GetWithOrigins (it always returned an empty array)

--- src/Infrastructure/Identity/Permissions/PermissionAuthorizationHandler.cs
+++ src/Infrastructure/Identity/Permissions/PermissionAuthorizationHandler.cs
@@ -1,4 +1,4 @@
-using DN.WebApi.Application.Abstractions.Services.Identity;
+using DN.WebApi.Application.Identity.Interfaces;
 using DN.WebApi.Infrastructure.Identity.Extensions;
 using Microsoft.AspNetCore.Authorization;
 
