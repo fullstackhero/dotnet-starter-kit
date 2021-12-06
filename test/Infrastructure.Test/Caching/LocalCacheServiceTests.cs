ceb71bb6ad455b9f597af471989e600660397b24
fretje <github@fretje.be>
2021-12-06 04:37:37 +0100

    Move cachingservicetests to caching folder

---
+++ test/Infrastructure.Test/Caching/LocalCacheServiceTests.cs
@@ -0,0 +1,11 @@
+﻿using DN.WebApi.Infrastructure.Caching;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Infrastructure.Test.Caching;
+
+public class LocalCacheServiceTests : CacheServiceTests<LocalCacheService>
+{
+    protected override LocalCacheService CreateCacheService() =>
+        new LocalCacheService(new MemoryCache(new MemoryCacheOptions()), NullLogger<LocalCacheService>.Instance);
+}
