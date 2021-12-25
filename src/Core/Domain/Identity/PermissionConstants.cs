1d13e34551572b823634b2250a445b2aee018253
Mukesh Murugan <iammukeshm@gmail.com>
2021-12-25 10:53:52 +0530

    added user permissions

--- src/Core/Domain/Identity/PermissionConstants.cs
+++ src/Core/Domain/Identity/PermissionConstants.cs
@@ -32,4 +32,16 @@ public partial class PermissionConstants
         public const string Delete = "Permissions.RoleClaims.Delete";
         public const string Search = "Permissions.RoleClaims.Search";
     }
+
+    [DisplayName("Users")]
+    [Description("Users Permissions")]
+    public static class Users
+    {
+        public const string View = "Permissions.Users.View";
+        public const string Create = "Permissions.Users.Create";
+        public const string Edit = "Permissions.Users.Edit";
+        public const string Delete = "Permissions.Users.Delete";
+        public const string Export = "Permissions.Users.Export";
+        public const string Search = "Permissions.Users.Search";
+    }
 }
