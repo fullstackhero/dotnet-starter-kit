35a91bc61cd8051e8b85fdbe5d879790ac288a13
Mukesh Murugan <iammukeshm@gmail.com>
2022-01-02 20:43:53 +0530

    user role assignment

--- src/Shared/Shared.DTOs/Identity/UserRoleDto.cs
+++ src/Shared/Shared.DTOs/Identity/UserRoleDto.cs
@@ -3,8 +3,7 @@ namespace DN.WebApi.Shared.DTOs.Identity;
 public class UserRoleDto
 {
     public string? RoleId { get; set; }
-
     public string? RoleName { get; set; }
-
+    public string? Description { get; set; }
     public bool Enabled { get; set; }
 }
