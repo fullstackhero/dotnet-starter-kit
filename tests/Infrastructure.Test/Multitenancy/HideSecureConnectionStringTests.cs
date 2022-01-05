5af11c6a981e85710e57419dda4d23512de9600b
xlogex <27079402+xlogex@users.noreply.github.com>
2022-01-05 01:43:49 +0100

    Unt Test

--- tests/Infrastructure.Test/Multitenancy/HideSecureConnectionStringTests.cs
+++ tests/Infrastructure.Test/Multitenancy/HideSecureConnectionStringTests.cs
@@ -4,24 +4,23 @@ namespace DN.WebApi.Infrastructure.Multitenancy.Tests
 {
     public class HideSecureConnectionStringTests
     {
+        private const string mssql = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=fullStackHeroDb;MultipleActiveResultSets=True;";
         [Fact]
         public void GetSecureConnectionStringTest()
         {
-            string mssql1 = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=fullStackHeroDb;Integrated Security=True;" +
-                         "MultipleActiveResultSets=True";
+            string mssql1 = mssql + ";Integrated Security=True;";
+            string mssql2 = mssql + ";user id=sa;password=pass;";
 
             string? res1 = HideSecureConnectionString.GetSecureConnectionString("mssql", mssql1);
-            Assert.Equal(res1, mssql1); // case 1
+            string? check1 = HideSecureConnectionString.GetSecureConnectionString("mssql", res1);
 
-            string mssql2 = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=fullStackHeroDb;user id=sa;password=pass;" +
-                         "MultipleActiveResultSets=True";
-            string mssql3 = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=fullStackHeroDb;user id=*******;password=*******;" +
-                         "MultipleActiveResultSets=True";
+            Assert.True(check1?.Equals(res1, StringComparison.InvariantCultureIgnoreCase), "MSSQL: CASE 1 - Integrated Security"); // CASE 1
 
             string? res2 = HideSecureConnectionString.GetSecureConnectionString("mssql", mssql2);
-            Assert.True(mssql3.Equals(res2, StringComparison.InvariantCultureIgnoreCase)); // case 2
+            string? check2 = HideSecureConnectionString.GetSecureConnectionString("mssql", res2);
 
-            // Assert.True(false, "This test needs an implementation");
+            Assert.True(check2?.Equals(res2, StringComparison.InvariantCultureIgnoreCase), "MSSQL: CASE 2 - Credentials"); // CASE 2
+            
         }
     }
 }
