fe9b82db35363059427d955a12ac6f1aef1cc039
fretje <github@fretje.be>
2022-01-12 00:07:48 +0100

    move tenantrepository into tenantmanagementrepository

--- src/Core/Application/Common/Persistence/ITenantManagementRepository.cs
+++ src/Core/Application/Common/Persistence/ITenantManagementRepository.cs
@@ -1,11 +1,11 @@
 ﻿using Ardalis.Specification;
 using DN.WebApi.Domain.Common.Contracts;
+using DN.WebApi.Domain.Multitenancy;
 
 namespace DN.WebApi.Application.Common.Persistence;
 
-// from Ardalis.Specification
-
-// TenantManagement Db
+// The Repository for the TenantManagement Db
+// I(Read)RepositoryBase<T> is from Ardalis.Specification
 
 public interface ITenantManagementRepository<T> : IRepositoryBase<T>
     where T : class, IAggregateRoot
@@ -14,5 +14,15 @@ public interface ITenantManagementRepository<T> : IRepositoryBase<T>
 
 public interface ITenantManagementReadRepository<T> : IReadRepositoryBase<T>
     where T : class, IAggregateRoot
+{
+}
+
+// Easy access to the TenantRepository
+
+public interface ITenantRepository : ITenantManagementRepository<Tenant>
+{
+}
+
+public interface ITenantReadRepository : ITenantManagementReadRepository<Tenant>
 {
 }
