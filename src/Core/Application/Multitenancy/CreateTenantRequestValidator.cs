c7583d3d4b1d63f4d64fe0a2d0622d5250ebefbc
Mukesh Murugan <iammukeshm@gmail.com>
2022-01-14 00:56:36 +0530

    updated validator - createTenantValidator

--- src/Core/Application/Multitenancy/CreateTenantRequestValidator.cs
+++ src/Core/Application/Multitenancy/CreateTenantRequestValidator.cs
@@ -12,17 +12,17 @@ public class CreateTenantRequestValidator : CustomValidator<CreateTenantRequest>
             .MustAsync(async (key, ct) => await repository.GetBySpecAsync(new TenantByKeySpec(key!), ct) is null)
             .WithMessage((_, key) => string.Format(localizer["tenant.alreadyexists"], key));
 
-        RuleFor(t => t.ConnectionString)
+        RuleFor(t => t.ConnectionString).Cascade(CascadeMode.Stop)
             .NotEmpty()
             .Must((t, cs) => string.IsNullOrWhiteSpace(cs) || tenantDbService.TryValidateConnectionString(cs, t.Key))
             .WithMessage(localizer["invalid.connectionstring"]);
 
-        RuleFor(t => t.Name)
+        RuleFor(t => t.Name).Cascade(CascadeMode.Stop)
             .NotEmpty()
             .MustAsync(async (name, ct) => await repository.GetBySpecAsync(new TenantByNameSpec(name!), ct) is null)
             .WithMessage((_, name) => string.Format(localizer["tenant.alreadyexists"], name));
 
-        RuleFor(t => t.AdminEmail)
+        RuleFor(t => t.AdminEmail).Cascade(CascadeMode.Stop)
             .NotEmpty().EmailAddress();
     }
 }
