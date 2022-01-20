5d3d78cd0b1d6383193b8be2ad50303cb9d1fa4a
Bjorn Bailleul <4582985+B-Virtual@users.noreply.github.com>
2022-01-20 13:41:36 +0100

    Update EntityDeletedEvent.cs

--- src/Core/Domain/Common/Events/EntityDeletedEvent.cs
+++ src/Core/Domain/Common/Events/EntityDeletedEvent.cs
@@ -1,7 +1,7 @@
 namespace FSH.WebApi.Domain.Common.Events;
 
 public class EntityDeletedEvent<T> : DomainEvent
-    where T : class
+    where T : IEntity
 {
     public EntityDeletedEvent(T entity) => Entity = entity;
 
