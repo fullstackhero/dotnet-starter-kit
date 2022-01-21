56f8cde66b4a6d1fd55c9a2c8ac12b933e6bfd3c
fretje <github@fretje.be>
2022-01-21 20:24:28 +0100

    PermissionDto: make permission non nullable
    also small cleanup

--- src/Core/Application/Identity/Roles/PermissionDto.cs
+++ src/Core/Application/Identity/Roles/PermissionDto.cs
@@ -2,6 +2,6 @@ namespace FSH.WebApi.Application.Identity.Roles;
 
 public class PermissionDto
 {
-    public string? Permission { get; set; }
+    public string Permission { get; set; } = default!;
     public string? Description { get; set; }
 }
