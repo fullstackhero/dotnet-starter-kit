b9d634cf788d9b79e19fd59c22d07e53c7ded6fc
fretje <github@fretje.be>
2022-01-26 14:49:12 +0100

    add cachekeyservice and made it "tenant aware"

---
+++ src/Core/Application/Common/Caching/CacheKeyServiceExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace FSH.WebApi.Application.Common.Caching;
+
+public static class CacheKeyServiceExtensions
+{
+    public static string GetCacheKey<TEntity>(this ICacheKeyService cacheKeyService, object id, bool includeTenantId = true)
+    where TEntity : IEntity =>
+        cacheKeyService.GetCacheKey(typeof(TEntity).Name, id, includeTenantId);
+}
