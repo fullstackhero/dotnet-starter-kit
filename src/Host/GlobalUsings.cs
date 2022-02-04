2622f1e90828e0c13703f90401d89fd8ce679beb
Szymon Sus <sus.szymon3@gmail.com>
2022-02-04 19:48:07 +0100

    Move global usings to GlobalUsings.cs

---
+++ src/Host/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using FSH.WebApi.Application.Common.Interfaces;
+global using FSH.WebApi.Application.Common.Models;
+global using FSH.WebApi.Infrastructure.Auth.Permissions;
+global using FSH.WebApi.Infrastructure.OpenApi;
+global using FSH.WebApi.Shared.Authorization;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using NSwag.Annotations;
