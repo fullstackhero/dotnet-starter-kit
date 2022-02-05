3b59efb7d7dbc48c427ebc279f0568a5c801022e
xlogex <27079402+xlogex@users.noreply.github.com>
2022-02-05 01:32:18 +0100

    mssql migration

--- src/Migrators/Migrators.MSSQL/Migrations/Tenant/20220205003010_IsRoot.Designer.cs
+++ src/Migrators/Migrators.MSSQL/Migrations/Tenant/20220205003010_IsRoot.Designer.cs
@@ -4,6 +4,7 @@ using FSH.WebApi.Infrastructure.Multitenancy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Migrators.MSSQL.Migrations.Tenant
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220205003010_IsRoot")]
+    partial class IsRoot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
