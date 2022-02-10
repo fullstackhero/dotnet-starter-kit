dd254dbb70e1cc50259606376dd1262ad5a82359
Žiga Serafin <rippedcarpet@gmail.com>
2022-02-10 22:34:46 +0100

    Changes to Email Sender... Now its generic and can be used for any model. Just create cshtml page and add @Model.ModelProperty

---
+++ src/Infrastructure/Identity/RegisterUserEmailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Infrastructure.Identity
+{
+    public class RegisterUserEmailModel
+    {
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Url { get; set; } = default!;
+    }
+}
