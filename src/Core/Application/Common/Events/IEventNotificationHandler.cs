21ccafb8407c92231f2aeec43901297618a55bc1
fretje <github@fretje.be>
2022-02-12 00:12:11 +0100

    add comments

--- src/Core/Application/Common/Events/IEventNotificationHandler.cs
+++ src/Core/Application/Common/Events/IEventNotificationHandler.cs
@@ -2,6 +2,7 @@
 
 namespace FSH.WebApi.Application.Common.Events;
 
+// This is just a shorthand to make it a bit easier to create event handlers for specific events.
 public interface IEventNotificationHandler<TEvent> : INotificationHandler<EventNotification<TEvent>>
     where TEvent : IEvent
 {
