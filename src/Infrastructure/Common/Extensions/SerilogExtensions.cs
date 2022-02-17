d32561dda2216c7a08b2297824083d438acae64c
xlogex <27079402+xlogex@users.noreply.github.com>
2022-02-17 03:31:55 +0100

    Update SerilogExtensions.cs (#491)
    * Update SerilogExtensions.cs
    
    * Update SerilogExtensions.cs

--- src/Infrastructure/Common/Extensions/SerilogExtensions.cs
+++ src/Infrastructure/Common/Extensions/SerilogExtensions.cs
@@ -4,8 +4,8 @@ namespace FSH.WebApi.Infrastructure.Common.Extensions;
 
 public static class SerilogExtensions
 {
-    public static void Refresh(this ILogger logger)
+    public static void Refresh(this ILogger? logger)
     {
-        if(logger != null && logger is not Serilog.Core.Logger) Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().WriteTo.Console().CreateLogger();
+        if(logger is { } and not Serilog.Core.Logger) Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().WriteTo.Console().CreateLogger();
     }
 }
