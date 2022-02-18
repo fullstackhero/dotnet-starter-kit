c488cb8d6d169b263de2233fe2a1525a4682efec
xlogex <27079402+xlogex@users.noreply.github.com>
2022-02-18 18:29:08 +0100

    Merge branch 'main' of https://github.com/fullstackhero/dotnet-webapi-boilerplate into fullstackhero-main

--- src/Core/Application/Common/Models/PaginationResponse.cs
+++ src/Core/Application/Common/Models/PaginationResponse.cs
@@ -25,17 +25,3 @@ public class PaginationResponse<T>
 
     public bool HasNextPage => CurrentPage < TotalPages;
 }
-
-public static class PaginationResponseExtensions
-{
-    public static async Task<PaginationResponse<TDestination>> PaginatedListAsync<T, TDestination>(
-        this IReadRepository<T> repository, ISpecification<T, TDestination> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
-        where T : class, IAggregateRoot
-        where TDestination : class, IDto
-    {
-        var list = await repository.ListAsync(spec, cancellationToken);
-        int count = await repository.CountAsync(spec, cancellationToken);
-
-        return new PaginationResponse<TDestination>(list, count, pageNumber, pageSize);
-    }
-}
