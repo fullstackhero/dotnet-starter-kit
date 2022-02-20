08e66103ac7a859a0fcac96f89068148af422514
xlogex <27079402+xlogex@users.noreply.github.com>
2022-02-20 23:28:29 +0100

    clean comment

--- src/Core/Application/Common/Models/PaginationResponseExtensions.cs
+++ src/Core/Application/Common/Models/PaginationResponseExtensions.cs
@@ -4,7 +4,7 @@ public static class PaginationResponseExtensions
 {
     public static async Task<PaginationResponse<TDestination>> PaginatedListAsync<T, TDestination>(
         this IReadRepositoryBase<T> repository, ISpecification<T, TDestination> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
-        where T : class // , IAggregateRoot
+        where T : class
         where TDestination : class, IDto
     {
         var list = await repository.ListAsync(spec, cancellationToken);
