e20466a5cdf3599302d25dd371f689e2f86af2e8
fretje <github@fretje.be>
2022-02-24 05:28:05 +0100

    SpecificationBuilderExtensions: Search All Fields: skip enums (#518)

--- src/Core/Application/Common/Specification/SpecificationBuilderExtensions.cs
+++ src/Core/Application/Common/Specification/SpecificationBuilderExtensions.cs
@@ -64,7 +64,9 @@ public static class SpecificationBuilderExtensions
             {
                 // search all fields (only first level)
                 foreach (var property in typeof(T).GetProperties()
-                    .Where(prop => Type.GetTypeCode(Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType) != TypeCode.Object))
+                    .Where(prop => (Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType) is { } propertyType
+                        && !propertyType.IsEnum
+                        && Type.GetTypeCode(propertyType) != TypeCode.Object))
                 {
                     var paramExpr = Expression.Parameter(typeof(T));
                     var propertyExpr = Expression.Property(paramExpr, property);
