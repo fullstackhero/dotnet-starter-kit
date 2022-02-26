03ea93b2e6de93dc52fee3676cb59757bc2b9d59
fretje <github@fretje.be>
2022-02-26 14:38:51 +0100

    security and exception middleware add check if response has started

--- src/Infrastructure/Middleware/ErrorResult.cs
+++ src/Infrastructure/Middleware/ErrorResult.cs
@@ -2,7 +2,7 @@
 
 public class ErrorResult
 {
-    public List<string>? Messages { get; set; } = new();
+    public List<string> Messages { get; set; } = new();
 
     public string? Source { get; set; }
     public string? Exception { get; set; }
