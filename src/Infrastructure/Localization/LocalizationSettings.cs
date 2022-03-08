8aea1d3caa19a2cac37ced19b80e3f1a86a61c11
fretje <github@fretje.be>
2022-03-08 18:12:31 +0100

    re-order settings

--- src/Infrastructure/Localization/LocalizationSettings.cs
+++ src/Infrastructure/Localization/LocalizationSettings.cs
@@ -1,9 +1,10 @@
 ﻿namespace FSH.WebApi.Infrastructure.Localization;
+
 public class LocalizationSettings
 {
     public bool? EnableLocalization { get; set; }
-    public string[]? SupportedCultures { get; set; }
     public string? ResourcesPath { get; set; }
+    public string[]? SupportedCultures { get; set; }
     public string? DefaultRequestCulture { get; set; }
     public bool? FallbackToParent { get; set; }
 }
