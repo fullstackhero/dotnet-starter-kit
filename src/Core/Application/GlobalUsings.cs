0661d5e9d08868bc98a1bf919b6ea60690f752b7
Muneeb Ejaz <mianmuneebajaz@gmail.com>
2022-03-27 15:11:54 +0500

    Update GlobalUsings.cs
    Fix build issue

--- src/Core/Application/GlobalUsings.cs
+++ src/Core/Application/GlobalUsings.cs
@@ -15,3 +15,4 @@ global using FSH.WebApi.Shared.Notifications;
 global using MediatR;
 global using Microsoft.Extensions.Localization;
 global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
