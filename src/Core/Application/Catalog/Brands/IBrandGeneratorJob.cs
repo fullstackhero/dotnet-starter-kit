8df9173d1ef6fa39c816987360b524016da15e3c
Chihab HAJJI <chihab.hajji@esprit.tn>
2022-03-28 11:52:13 +0100

    Fixed typo in line 10 (#582)
    from "radom" to "random"

--- src/Core/Application/Catalog/Brands/IBrandGeneratorJob.cs
+++ src/Core/Application/Catalog/Brands/IBrandGeneratorJob.cs
@@ -7,6 +7,6 @@ public interface IBrandGeneratorJob : IScopedService
     [DisplayName("Generate Random Brand example job on Queue notDefault")]
     Task GenerateAsync(int nSeed, CancellationToken cancellationToken);
 
-    [DisplayName("removes all radom brands created example job on Queue notDefault")]
+    [DisplayName("removes all random brands created example job on Queue notDefault")]
     Task CleanAsync(CancellationToken cancellationToken);
 }
