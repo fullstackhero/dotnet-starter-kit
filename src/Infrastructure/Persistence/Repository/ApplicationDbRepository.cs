50715540fefa7b7dc6a1afd1b6827ec8d56b1596
fretje <github@fretje.be>
2022-04-18 04:16:20 +0200

    ApplicationDbRepository.ApplySpecification => only use mapster when no selector is defined (#606)

--- src/Infrastructure/Persistence/Repository/ApplicationDbRepository.cs
+++ src/Infrastructure/Persistence/Repository/ApplicationDbRepository.cs
@@ -18,7 +18,10 @@ public class ApplicationDbRepository<T> : RepositoryBase<T>, IReadRepository<T>,
 
     // We override the default behavior when mapping to a dto.
     // We're using Mapster's ProjectToType here to immediately map the result from the database.
+    // This is only done when no Selector is defined, so regular specifications with a selector also still work.
     protected override IQueryable<TResult> ApplySpecification<TResult>(ISpecification<T, TResult> specification) =>
-        ApplySpecification(specification, false)
-            .ProjectToType<TResult>();
+        specification.Selector is not null
+            ? base.ApplySpecification(specification)
+            : ApplySpecification(specification, false)
+                .ProjectToType<TResult>();
 }
