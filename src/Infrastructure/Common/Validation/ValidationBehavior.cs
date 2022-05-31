2081574b278245ed13c0d9d83bf0db15c0c0b6ef
fretje <github@fretje.be>
2022-05-31 16:53:03 +0200

    had to add context with serviceprovider to support InjectValidator()
    but other than that can update fluentvalidation now

--- src/Infrastructure/Common/Validation/ValidationBehavior.cs
+++ src/Infrastructure/Common/Validation/ValidationBehavior.cs
@@ -7,12 +7,21 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
     where TRequest : IRequest<TResponse>
 {
     private readonly IEnumerable<IValidator<TRequest>> _validators;
+    private readonly IServiceProvider _sp;
 
-    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators, IServiceProvider sp) =>
+        (_validators, _sp) = (validators, sp);
 
     public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
     {
-        var failures = (await Task.WhenAll(_validators.Select(v => v.ValidateAsync(request))))
+        var context = new ValidationContext<TRequest>(request);
+        context.SetServiceProvider(_sp); // to be able to use InjectValidator()
+
+        var results = await Task.WhenAll(
+            _validators.Select(
+                v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = results
             .SelectMany(result => result.Errors)
             .Where(f => f != null)
             .ToList();
