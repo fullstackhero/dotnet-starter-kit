67f045b8f5b57fad95c11cd00112b7aacd6adaf6
Mukesh Murugan <iammukeshm@gmail.com>
2023-04-04 22:28:18 +0530

    revert

--- src/Infrastructure/Multitenancy/TenantHealthCheck.cs
+++ src/Infrastructure/Multitenancy/TenantHealthCheck.cs
@@ -1,21 +1,13 @@
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Logging;
 
 namespace FSH.WebApi.Infrastructure.Multitenancy;
 
 public class TenantHealthCheck : IHealthCheck
 {
-    private readonly ILogger<TenantHealthCheck> _logger;
-
-    public TenantHealthCheck(ILogger<TenantHealthCheck> logger)
-    {
-        _logger = logger;
-    }
-
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
+        // Descoped
         var check = new HealthCheckResult(HealthStatus.Healthy);
-        _logger.LogInformation($"Status is {check.Status}");
         return Task.FromResult(check);
     }
 }
