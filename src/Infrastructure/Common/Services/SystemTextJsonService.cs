15595a59afae3b6ab2caffe4a7297f91188a632f
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-11 23:13:00 +0300

    Used System.Text.Json instead of Newtonsoft for better performance

---
+++ src/Infrastructure/Common/Services/SystemTextJsonService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using FSH.WebApi.Application.Common.Interfaces;
+
+namespace FSH.WebApi.Infrastructure.Common.Services;
+
+public class SystemTextJsonService : ISerializerService
+{
+    private readonly JsonSerializerOptions _options;
+
+    public SystemTextJsonService()
+    {
+        _options = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters =
+            {
+                new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+            },
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+    }
+
+    public T Deserialize<T>(string text)
+    {
+        return JsonSerializer.Deserialize<T>(text, _options) ?? default!;
+    }
+
+    public string Serialize<T>(T obj)
+    {
+        return JsonSerializer.Serialize(obj, _options);
+    }
+
+    public string Serialize<T>(T obj, Type type)
+    {
+        return JsonSerializer.Serialize(obj, type, _options);
+    }
+}
