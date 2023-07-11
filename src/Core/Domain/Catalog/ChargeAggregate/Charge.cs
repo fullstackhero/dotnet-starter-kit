63b3fee3a86b9a1ce3e00a7479246af80368f524
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-12 00:04:28 +0300

    added example chargeAggregate for event sourcing

---
+++ src/Core/Domain/Catalog/ChargeAggregate/Charge.cs
@@ -0,0 +1,46 @@
+﻿using FSH.WebApi.Domain.Catalog.ChargeAggregate.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Domain.Catalog.ChargeAggregate;
+
+public class Charge
+{
+    public Charge(Guid id, Guid userId, int meterValue)
+    {
+        Id = id;
+        UserId = userId;
+        MeterValue = meterValue;
+    }
+
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public int MeterValue { get; private set; }
+    public ChargeStatus Status { get; private set; } = ChargeStatus.Initiated;
+
+    public void Apply(ChargeInitiatedEvent @event)
+    {
+        Id = @event.ChargeId;
+        UserId = @event.UserId;
+        MeterValue = @event.MeterValue;
+        Status = ChargeStatus.Initiated;
+    }
+
+    public void Apply(ChargeStartedEvent @event)
+    {
+        Status = ChargeStatus.Active;
+    }
+
+    public void Apply(ChargeReceivedMeterValueEvent @event)
+    {
+        MeterValue = @event.MeterValue;
+    }
+
+    public void Apply(ChargeStoppedEvent @event)
+    {
+        Status = ChargeStatus.Stopped;
+    }
+}
---
+++ src/Core/Domain/Catalog/ChargeAggregate/Events/ChargeInitiatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Domain.Catalog.ChargeAggregate.Events;
+
+public class ChargeInitiatedEvent : DomainEvent
+{
+    public ChargeInitiatedEvent(Guid chargeId, Guid userId, int meterValue)
+    {
+        ChargeId = chargeId;
+        UserId = userId;
+        MeterValue = meterValue;
+    }
+
+    public Guid ChargeId { get; }
+    public Guid UserId { get; }
+    public int MeterValue { get; }
+}
