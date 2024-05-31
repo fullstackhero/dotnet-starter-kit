1bea4823c4520229189c6cbb474ffcf729283cb4
Mukesh Murugan <iammukeshm@gmail.com>
2024-05-31 14:43:50 +0530

    add docker compose

--- api/framework/Infrastructure/Caching/Extensions.cs
+++ api/framework/Infrastructure/Caching/Extensions.cs
@@ -9,29 +9,25 @@ internal static class Extensions
     private static readonly ILogger _logger = Log.ForContext(typeof(Extensions));
     internal static IServiceCollection ConfigureCaching(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddTransient<ICacheService, DistributedCacheService>();
         var cacheOptions = configuration.GetSection(nameof(CacheOptions)).Get<CacheOptions>();
-        if (cacheOptions == null) return services;
-
-        if (!string.IsNullOrEmpty(cacheOptions.Redis))
-        {
-            _logger.Information("configuring redis cache.");
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = cacheOptions.Redis;
-                options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
-                {
-                    AbortOnConnectFail = true,
-                    EndPoints = { cacheOptions.Redis! }
-                };
-            });
-        }
-        else
+        if (cacheOptions == null || string.IsNullOrEmpty(cacheOptions.Redis))
         {
             _logger.Information("configuring memory cache.");
             services.AddDistributedMemoryCache();
+            return services;
         }
 
-        services.AddTransient<ICacheService, DistributedCacheService>();
+        _logger.Information("configuring redis cache.");
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = cacheOptions.Redis;
+            options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
+            {
+                AbortOnConnectFail = true,
+                EndPoints = { cacheOptions.Redis! }
+            };
+        });
 
         return services;
     }
