bace35310a2b42d05f537a782166af3242362035
Vipul Malhotra <vipulm124@gmail.com>
2024-06-05 17:28:39 +0530

    removed product read handler

--- api/modules/Catalog/Catalog.Domain/Events/ProductRead.cs
+++
@@ -1,7 +0,0 @@
-using FSH.Framework.Core.Domain.Events;
-
-namespace FSH.WebApi.Catalog.Domain.Events;
-public sealed record ProductRead : DomainEvent
-{
-    public Product? Product { get; set; }
-}
