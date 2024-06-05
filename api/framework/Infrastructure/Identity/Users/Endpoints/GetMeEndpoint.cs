4ec5edfc70eb8563d3db90a2ccfed3204fa700ca
wtmaxim <maxime.derame@hotmail.fr>
2024-06-05 10:05:09 +0200

    add get me endpoint

---
+++ api/framework/Infrastructure/Identity/Users/Endpoints/GetMeEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FSH.Framework.Core.Exceptions;
+using System.Security.Claims;
+using FSH.Framework.Core.Identity.Users.Abstractions;
+using FSH.Framework.Infrastructure.Auth.Policy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace FSH.Framework.Infrastructure.Identity.Users.Endpoints;
+public static class GetMeEndpoint
+{
+    internal static RouteHandlerBuilder MapGetMeEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+
+        return endpoints.MapGet("/me", async (ClaimsPrincipal user, IUserService service, CancellationToken cancellationToken) =>
+        {
+            if (user.GetUserId() is not { } userId || string.IsNullOrEmpty(userId))
+            {
+                throw new UnauthorizedException();
+            }
+
+            var userDetails = await service.GetAsync(userId, cancellationToken);
+            return Results.Ok(userDetails);
+        })
+        .WithName("GetMeEndpoint")
+        .WithSummary("Get current user information based on token")
+        .RequirePermission("Permissions.Users.View")
+        .WithDescription("Get current user information based on token");
+    }
+}
+
