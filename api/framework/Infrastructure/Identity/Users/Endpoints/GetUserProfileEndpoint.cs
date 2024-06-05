a59d10e39c9c48a669d775974bd5fc8b1414b7d0
wtmaxim <maxime.derame@hotmail.fr>
2024-06-05 10:59:06 +0200

    clean

--- api/framework/Infrastructure/Identity/Users/Endpoints/GetUserProfileEndpoint.cs
+++ api/framework/Infrastructure/Identity/Users/Endpoints/GetUserProfileEndpoint.cs
@@ -11,7 +11,6 @@ public static class GetUserProfileEndpoint
 {
     internal static RouteHandlerBuilder MapGetMeEndpoint(this IEndpointRouteBuilder endpoints)
     {
-
         return endpoints.MapGet("/profile", async (ClaimsPrincipal user, IUserService service, CancellationToken cancellationToken) =>
         {
             if (user.GetUserId() is not { } userId || string.IsNullOrEmpty(userId))
@@ -20,7 +19,6 @@ public static class GetUserProfileEndpoint
             }
 
             return await service.GetAsync(userId, cancellationToken);
-            //return Results.Ok(userDetails);
         })
         .WithName("GetMeEndpoint")
         .WithSummary("Get current user information based on token")
