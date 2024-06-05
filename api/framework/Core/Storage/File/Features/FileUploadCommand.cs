7f35d36923a4067158a41c29156f494dd1fbd523
Maxime <maxime.derame@hotmail.fr>
2024-06-05 18:09:16 +0200

    add file storage service (#938)
    * add file storage service
    
    * change FileStorage into Storage/File
    
    * change FileStorage into Storage/File
    
    * fix build
    
    * fix warnings

---
+++ api/framework/Core/Storage/File/Features/FileUploadCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace FSH.Framework.Core.Storage.File.Features;
+
+public class FileUploadRequestCommand : IRequest<FileUploadResponse>
+{
+    public string Name { get; set; } = default!;
+    public string Extension { get; set; } = default!;
+    public string Data { get; set; } = default!;
+}
+
