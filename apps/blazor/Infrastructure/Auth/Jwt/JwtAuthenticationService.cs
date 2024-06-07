f98873a3f0fc50406ee8cda4f05966e889c15c5f
Mukesh Murugan <iammukeshm@gmail.com>
2024-06-08 02:29:38 +0530

    initial blazor changes

---
+++ apps/blazor/Infrastructure/Auth/Jwt/JwtAuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Api;
+
+namespace Infrastructure.Auth.Jwt;
+public class JwtAuthenticationService : IAuthenticationService
+{
+
+    public Task<bool> LoginAsync(string tenantId, TokenGenerationCommand request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NavigateToExternalLogin(string returnUrl)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReLoginAsync(string returnUrl)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ apps/blazor/Infrastructure/Auth/Jwt/PersistentAuthenticationStateProvider.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Infrastructure.Auth.Jwt;
+
+// This is a client-side AuthenticationStateProvider that determines the user's authentication state by
+// looking for data persisted in the page when it was rendered on the server. This authentication state will
+// be fixed for the lifetime of the WebAssembly application. So, if the user needs to log in or out, a full
+// page reload is required.
+//
+// This only provides a user name and email for display purposes. It does not actually include any tokens
+// that authenticate to the server when making subsequent requests. That works separately using a
+// cookie that will be included on HttpClient requests to the server.
+internal sealed class PersistentAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private static readonly Task<AuthenticationState> defaultUnauthenticatedTask =
+        Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+
+    private readonly Task<AuthenticationState> authenticationStateTask = defaultUnauthenticatedTask;
+
+    public PersistentAuthenticationStateProvider(PersistentComponentState state)
+    {
+        if (!state.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+        {
+            return;
+        }
+
+        authenticationStateTask = Task.FromResult(new AuthenticationState(userInfo.ToClaimsPrincipal()));
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync() => authenticationStateTask;
+}
