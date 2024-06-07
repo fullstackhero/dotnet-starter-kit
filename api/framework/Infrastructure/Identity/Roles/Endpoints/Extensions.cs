296ae2852e8e8c861e691dc14d6a76e5f829bac1
Maxime <maxime.derame@hotmail.fr>
2024-06-07 23:04:54 +0200

    added roles endpoints (#940)
    * init roleService
    
    * wip
    
    * AddTransientrole service

---
+++ api/framework/Infrastructure/Identity/Roles/Endpoints/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace FSH.Framework.Infrastructure.Identity.Roles.Endpoints;
+
+internal static class Extensions
+{
+    public static IEndpointRouteBuilder MapRoleEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGetRoleEndpoint();
+        app.MapGetRolesEndpoint();
+        app.MapDeleteRoleEndpoint();
+        app.MapCreateOrUpdateRoleEndpoint();
+        return app;
+    }
+}
+
---
+++ api/framework/Infrastructure/Identity/Roles/Endpoints/GetRoleEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using FSH.Framework.Core.Identity.Roles;
+using FSH.Framework.Infrastructure.Auth.Policy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace FSH.Framework.Infrastructure.Identity.Roles.Endpoints;
+
+public static class GetRoleByIdEndpoint
+{
+    public static RouteHandlerBuilder MapGetRoleEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        return endpoints.MapGet("/api/roles/{id}", async (string id, IRoleService roleService) =>
+        {
+            return await roleService.GetRoleAsync(id);
+        })
+        .WithName("GetRoleById")
+        .WithSummary("Get role details by ID")
+        .RequirePermission("Permissions.Roles.View")
+        .WithDescription("Retrieve the details of a role by its ID.");
+    }
+}
+
