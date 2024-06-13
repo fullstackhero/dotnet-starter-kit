8ee090259ba36f337b00e0eca15d2f9bd8371de6
Vipul Malhotra <vipulm124@gmail.com>
2024-06-14 00:21:56 +0530

    removed itemgroup from proj file

--- api/modules/Todo/Features/Update/v1/UpdateTodoEndpoint.cs
+++ api/modules/Todo/Features/Update/v1/UpdateTodoEndpoint.cs
@@ -10,12 +10,13 @@ public static class UpdateTodoEndpoint
 {
     internal static RouteHandlerBuilder MapTodoItemUpdationEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        return endpoints.MapPut("/{id:guid}", async (Guid id, UpdateTodoCommand request, ISender mediator) =>
-        {
-            if (id != request.Id) return Results.BadRequest();
-            var response = await mediator.Send(request);
-            return Results.Ok(response);
-        })
+        return endpoints.
+            MapPut("/{id:guid}", async (Guid id, UpdateTodoCommand request, ISender mediator) =>
+            {
+                if (id != request.Id) return Results.BadRequest();
+                var response = await mediator.Send(request);
+                return Results.Ok(response);
+            })
             .WithName(nameof(UpdateTodoEndpoint))
             .WithSummary("Updates a todo item")
             .WithDescription("Updated a todo item")
