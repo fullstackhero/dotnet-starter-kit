f891b6be2cdc1969c20550e971ede4aaf60cd27d
wtmaxim <maxime.derame@hotmail.fr>
2024-06-24 09:00:28 +0200

    wip

--- api/framework/Core/Specifications/ListSpecification.cs
+++ api/framework/Core/Specifications/ListSpecification.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq.Expressions;
 using Ardalis.Specification;
 using FSH.Framework.Core.Paging;
-using System.Linq;
 
 namespace FSH.Framework.Core.Specifications;
 
@@ -12,6 +11,7 @@ public class ListSpecification<T, TDto> : Specification<T, TDto> where T : class
         ApplyPagination(filter.PageNumber, filter.PageSize);
         ApplySorting(filter.AdvancedFilter);
         ApplySearch(filter.AdvancedSearch);
+        //ApplyAdvancedFilter(filter.AdvancedFilter);
     }
 
     private void ApplyPagination(int pageNumber, int pageSize)
@@ -37,10 +37,14 @@ public class ListSpecification<T, TDto> : Specification<T, TDto> where T : class
 
         switch (advancedFilter.Operator)
         {
-            case FilterOperator.EQ:
+            case FilterOperator.EQ: // Utilisé pour OrderBy
+            case FilterOperator.LT: // Utilisé pour OrderBy
+            case FilterOperator.LTE: // Utilisé pour OrderBy
+            case FilterOperator.GT: // Utilisé pour OrderBy
+            case FilterOperator.GTE: // Utilisé pour OrderBy
                 Query.OrderBy(lambda);
                 break;
-            case FilterOperator.NEQ: 
+            case FilterOperator.NEQ: // Utilisé pour OrderByDescending
                 Query.OrderByDescending(lambda);
                 break;
             default:
@@ -76,5 +80,62 @@ public class ListSpecification<T, TDto> : Specification<T, TDto> where T : class
 
         var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
         Query.Where(lambda);
+    }
+
+    private void ApplyAdvancedFilter(Filter? advancedFilter)
+    {
+        if (advancedFilter == null) return;
+
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var body = CreateFilterExpression(parameter, advancedFilter);
+
+        if (body == null) return;
+
+        var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+        Query.Where(lambda);
+    }
+
+    private Expression? CreateFilterExpression(ParameterExpression parameter, Filter filter)
+    {
+        Expression? body = null;
+
+        if (filter.Filters != null && filter.Filters.Any() || filter.Logic != null)
+        {
+            foreach (var subFilter in filter.Filters)
+            {
+                var subExpression = CreateFilterExpression(parameter, subFilter);
+                if (subExpression == null) continue;
+
+                body = body == null ? subExpression :
+                       filter.Logic switch
+                       {
+                           FilterLogic.AND => Expression.AndAlso(body, subExpression),
+                           FilterLogic.OR => Expression.OrElse(body, subExpression),
+                           FilterLogic.XOR => Expression.ExclusiveOr(body, subExpression),
+                           _ => throw new NotSupportedException($"Logic '{filter.Logic}' is not supported.")
+                       };
+            }
+        }
+        else if (filter.Field != null && filter.Operator != null)
+        {
+            var property = Expression.Property(parameter, filter.Field);
+            var constant = Expression.Constant(Convert.ChangeType(filter.Value, property.Type));
+
+            body = filter.Operator switch
+            {
+                FilterOperator.EQ => Expression.Equal(property, constant),
+                FilterOperator.NEQ => Expression.NotEqual(property, constant),
+                FilterOperator.LT => Expression.LessThan(property, constant),
+                FilterOperator.LTE => Expression.LessThanOrEqual(property, constant),
+                FilterOperator.GT => Expression.GreaterThan(property, constant),
+                FilterOperator.GTE => Expression.GreaterThanOrEqual(property, constant),
+                FilterOperator.STARTSWITH => Expression.Call(property, typeof(string).GetMethod("StartsWith", new[] { typeof(string) })!, constant),
+                FilterOperator.ENDSWITH => Expression.Call(property, typeof(string).GetMethod("EndsWith", new[] { typeof(string) })!, constant),
+                FilterOperator.CONTAINS => Expression.Call(property, typeof(string).GetMethod("Contains", new[] { typeof(string) })!, constant),
+                _ => throw new NotSupportedException($"Operator '{filter.Operator}' is not supported.")
+            };
+        }
+
+        return body;
     }
 }
