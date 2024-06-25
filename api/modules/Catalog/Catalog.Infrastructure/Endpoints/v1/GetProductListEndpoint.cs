e051abf5510ed07b8f887033e3ae6a7d21c3c4dd
wtmaxim <maxime.derame@hotmail.fr>
2024-06-25 10:14:15 +0200

    fix build

--- api/modules/Catalog/Catalog.Infrastructure/Endpoints/v1/GetProductListEndpoint.cs
+++ api/modules/Catalog/Catalog.Infrastructure/Endpoints/v1/GetProductListEndpoint.cs
@@ -5,24 +5,27 @@ using FSH.WebApi.Catalog.Application.Products.GetList.v1;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace FSH.WebApi.Catalog.Infrastructure.Endpoints.v1;
+
 public static class GetProductListEndpoint
 {
     internal static RouteHandlerBuilder MapGetProductListEndpoint(this IEndpointRouteBuilder endpoints)
     {
         return endpoints
-            .MapGet("/", async (ISender mediator, int pageNumber = 1, int pageSize = 10) =>
+            .MapPost("/search", async (ISender mediator, [FromBody] PaginationFilter filter) =>
             {
-                var response = await mediator.Send(new GetProductListRequest(pageNumber, pageSize));
+                var response = await mediator.Send(new GetProductListRequest(filter));
                 return Results.Ok(response);
             })
             .WithName(nameof(GetProductListEndpoint))
-            .WithSummary("gets a list of products")
-            .WithDescription("gets a list of products")
+            .WithSummary("Gets a list of products")
+            .WithDescription("Gets a list of products with pagination and filtering support")
             .Produces<PagedList<GetProductResponse>>()
             .RequirePermission("Permissions.Products.View")
             .MapToApiVersion(1);
     }
 }
+
