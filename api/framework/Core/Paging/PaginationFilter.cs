399a2396ecd4c8341918aa283f4b7af8c431bd90
wtmaxim <maxime.derame@hotmail.fr>
2024-06-25 10:03:41 +0200

    added sorting & searching

--- api/framework/Core/Paging/PaginationFilter.cs
+++ api/framework/Core/Paging/PaginationFilter.cs
@@ -5,4 +5,5 @@ public class PaginationFilter : BaseFilter
     public int PageNumber { get; set; }
 
     public int PageSize { get; set; } = int.MaxValue;
+    public string[]? OrderBy { get; set; }
 }
--- api/modules/Todo/Features/GetList/v1/GetTodoListEndpoint.cs
+++ api/modules/Todo/Features/GetList/v1/GetTodoListEndpoint.cs
@@ -3,23 +3,25 @@ using FSH.Framework.Infrastructure.Auth.Policy;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace FSH.WebApi.Todo.Features.GetList.v1;
+
 public static class GetTodoListEndpoint
 {
     internal static RouteHandlerBuilder MapGetTodoListEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        return endpoints.MapPost("/search", (ISender mediator, PaginationFilter filter) =>
+        return endpoints.MapPost("/search", async (ISender mediator, [FromBody] PaginationFilter filter) =>
         {
-            var response = mediator.Send(new GetTodoListRequest(filter));
+            var response = await mediator.Send(new GetTodoListRequest(filter));
             return Results.Ok(response);
         })
-            .WithName(nameof(GetTodoListEndpoint))
-            .WithSummary("gets a list of todo items with paging support")
-            .WithDescription("gets a list of todo items with paging support")
-            .Produces<PagedList<TodoDto>>()
-            .RequirePermission("Permissions.Todos.View")
-            .MapToApiVersion(1);
+        .WithName(nameof(GetTodoListEndpoint))
+        .WithSummary("Gets a list of todo items with paging support")
+        .WithDescription("Gets a list of todo items with paging support")
+        .Produces<PagedList<TodoDto>>()
+        .RequirePermission("Permissions.Todos.View")
+        .MapToApiVersion(1);
     }
 }
