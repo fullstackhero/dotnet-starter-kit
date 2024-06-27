8e72a64100fc67b360f84c65d0186e7d6c66ffa4
wtmaxim <maxime.derame@hotmail.fr>
2024-06-27 13:20:52 +0200

    init signalr

---
+++ api/framework/Infrastructure/Notifications/NotificationHub.cs
@@ -0,0 +1,43 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using FSH.Framework.Core.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace FSH.Framework.Infrastructure.Notifications;
+
+[Authorize]
+public class NotificationHub : Hub
+{
+    private readonly ITenantInfo? _currentTenant;
+    private readonly ILogger<NotificationHub> _logger;
+
+    public NotificationHub(ITenantInfo? currentTenant, ILogger<NotificationHub> logger)
+    {
+        _currentTenant = currentTenant;
+        _logger = logger;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        if (_currentTenant is null)
+        {
+            throw new UnauthorizedException("Authentication Failed.");
+        }
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"GroupTenant-{_currentTenant.Id}");
+
+        await base.OnConnectedAsync();
+
+        _logger.LogInformation("A client connected to NotificationHub: {connectionId}", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"GroupTenant-{_currentTenant!.Id}");
+
+        await base.OnDisconnectedAsync(exception);
+
+        _logger.LogInformation("A client disconnected from NotificationHub: {connectionId}", Context.ConnectionId);
+    }
+}
