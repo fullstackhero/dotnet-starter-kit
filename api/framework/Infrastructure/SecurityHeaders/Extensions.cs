a804485aae0e5fc169d434fd2b147c7bb2ae8cf7
kienct <kingofdespair69@gmail.com>
2024-06-28 12:13:29 +0700

    change rate limit to options and add security header

---
+++ api/framework/Infrastructure/SecurityHeaders/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace FSH.Framework.Infrastructure.SecurityHeaders;
+
+public static class Extensions
+{
+    internal static IServiceCollection ConfigureSecurityHeaders(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<SecurityHeaderOptions>(config.GetSection(nameof(SecurityHeaderOptions)));
+
+        return services;
+    }
+    
+    internal static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder app)
+    {
+        var options = app.ApplicationServices.GetRequiredService<IOptions<SecurityHeaderOptions>>().Value;
+        
+        if (options.Enable)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (!context.Response.HasStarted)
+                {
+                    if (!string.IsNullOrWhiteSpace(options.Headers.XFrameOptions))
+                    {
+                        context.Response.Headers.XFrameOptions = options.Headers.XFrameOptions;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(options.Headers.XContentTypeOptions))
+                    {
+                        context.Response.Headers.XContentTypeOptions = options.Headers.XContentTypeOptions;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(options.Headers.ReferrerPolicy))
+                    {
+                        context.Response.Headers.Referer = options.Headers.ReferrerPolicy;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(options.Headers.PermissionsPolicy))
+                    {
+                        context.Response.Headers["Permissions-Policy"] = options.Headers.PermissionsPolicy;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(options.Headers.XXSSProtection))
+                    {
+                        context.Response.Headers.XXSSProtection = options.Headers.XXSSProtection;
+                    }
+                
+                    if (!string.IsNullOrWhiteSpace(options.Headers.ContentSecurityPolicy))
+                    {
+                        context.Response.Headers.ContentSecurityPolicy = options.Headers.ContentSecurityPolicy;
+                    }
+                
+                    if (!string.IsNullOrWhiteSpace(options.Headers.StrictTransportSecurity))
+                    {
+                        context.Response.Headers.StrictTransportSecurity = options.Headers.StrictTransportSecurity;
+                    }
+                }
+            
+                await next.Invoke();
+            });
+        }
+        
+        return app;
+    }
+}
---
+++ api/framework/Infrastructure/SecurityHeaders/SecurityHeaders.cs
@@ -0,0 +1,12 @@
+﻿namespace FSH.Framework.Infrastructure.SecurityHeaders;
+
+public class SecurityHeaders
+{
+    public string? XContentTypeOptions { get; set; }
+    public string? ReferrerPolicy { get; set; }
+    public string? XXSSProtection { get; set; }
+    public string? XFrameOptions { get; set; }
+    public string? ContentSecurityPolicy { get; set; }
+    public string? PermissionsPolicy { get; set; }
+    public string? StrictTransportSecurity { get; set; }
+}
