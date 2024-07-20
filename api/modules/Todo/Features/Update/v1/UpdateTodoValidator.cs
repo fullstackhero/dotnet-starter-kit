14dadd24b4e460ebcafc4af8bfa63c2a7ab8c48b
Mukesh Murugan <iammukeshm@gmail.com>
2024-07-20 08:22:10 +0530

    remove title validation

--- api/modules/Todo/Features/Update/v1/UpdateTodoValidator.cs
+++ api/modules/Todo/Features/Update/v1/UpdateTodoValidator.cs
@@ -1,17 +1,12 @@
 ﻿using FluentValidation;
 using FSH.WebApi.Todo.Persistence;
-using Microsoft.EntityFrameworkCore;
 
 namespace FSH.WebApi.Todo.Features.Update.v1;
 public class UpdateTodoValidator : AbstractValidator<UpdateTodoCommand>
 {
     public UpdateTodoValidator(TodoDbContext context)
     {
-        RuleFor(p => p.Title)
-            .NotEmpty()
-            .MustAsync(async (title, token) => await context.Todos.AllAsync(a => a.Title != title, token).ConfigureAwait(false))
-            .WithMessage((_, name) => "todo item title '{PropertyValue}' already exists.");
-
+        RuleFor(p => p.Title).NotEmpty();
         RuleFor(p => p.Note).NotEmpty();
     }
 }
