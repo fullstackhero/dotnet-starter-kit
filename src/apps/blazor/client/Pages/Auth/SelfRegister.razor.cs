f617569754f9a4437b10fdde855ee209c3679f15
Mukesh Murugan <iammukeshm@gmail.com>
2024-07-23 10:07:14 +0530

    added auth pages

---
+++ src/apps/blazor/client/Pages/Auth/SelfRegister.razor.cs
@@ -0,0 +1,57 @@
+﻿using FSH.Starter.Blazor.Client.Components;
+using FSH.Starter.Blazor.Infrastructure.Api;
+using FSH.Starter.Blazor.Shared;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FSH.Starter.Blazor.Client.Pages.Auth;
+
+public partial class SelfRegister
+{
+    private readonly RegisterUserCommand _createUserRequest = new();
+    private FshValidation? _customValidation;
+    private bool BusySubmitting { get; set; }
+
+    [Inject]
+    private IApiClient UsersClient { get; set; } = default!;
+
+    private string Tenant { get; set; } = TenantConstants.Root.Id;
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    private async Task SubmitAsync()
+    {
+        BusySubmitting = true;
+
+        var response = await ApiHelper.ExecuteCallGuardedAsync(
+            () => UsersClient.RegisterUserEndpointAsync(_createUserRequest),
+            Toast,
+            _customValidation);
+
+        if (response != null)
+        {
+            Toast.Add(response.UserId, Severity.Info);
+            Navigation.NavigateTo("/login");
+        }
+
+        BusySubmitting = false;
+    }
+
+    private void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
