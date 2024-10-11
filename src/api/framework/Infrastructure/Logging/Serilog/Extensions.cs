58af19c5c72e35c37338d930104c1fb22f7ad908
Mukesh Murugan <iammukeshm@gmail.com>
2024-10-11 11:41:23 +0530

    fixes #4583

--- src/api/framework/Infrastructure/Logging/Serilog/Extensions.cs
+++ src/api/framework/Infrastructure/Logging/Serilog/Extensions.cs
@@ -12,8 +12,39 @@ public static class Extensions
         ArgumentNullException.ThrowIfNull(builder);
         builder.Host.UseSerilog((context, logger) =>
         {
+            logger.WriteTo.OpenTelemetry(options =>
+            {
+                try
+                {
+                    options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+                    var headers = builder.Configuration["OTEL_EXPORTER_OTLP_HEADERS"]?.Split(',') ?? [];
+                    foreach (var header in headers)
+                    {
+                        var (key, value) = header.Split('=') switch
+                        {
+                        [string k, string v] => (k, v),
+                            var v => throw new Exception($"Invalid header format {v}")
+                        };
+
+                        options.Headers.Add(key, value);
+                    }
+                    options.ResourceAttributes.Add("service.name", "apiservice");
+                    //To remove the duplicate issue, we can use the below code to get the key and value from the configuration
+                    var (otelResourceAttribute, otelResourceAttributeValue) = builder.Configuration["OTEL_RESOURCE_ATTRIBUTES"]?.Split('=') switch
+                    {
+                    [string k, string v] => (k, v),
+                        _ => throw new Exception($"Invalid header format {builder.Configuration["OTEL_RESOURCE_ATTRIBUTES"]}")
+                    };
+                    options.ResourceAttributes.Add(otelResourceAttribute, otelResourceAttributeValue);
+                }
+                catch
+                {
+                    //ignore
+                }
+            });
             logger.ReadFrom.Configuration(context.Configuration);
             logger.Enrich.FromLogContext();
+            logger.Enrich.WithCorrelationId();
             logger
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
