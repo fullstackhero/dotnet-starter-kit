90c86abdff8b2c154a00fb62d742c2ba0db0cc2c
Jacek Michalski <michalski.jck@gmail.com>
2024-11-16 10:26:42 +0100

    Add BrandConfiguration for EF Core in Catalog project
    Introduce BrandConfiguration class to configure the Brand entity
    for Entity Framework Core. This includes enabling multi-tenancy,
    setting the primary key, and specifying maximum lengths for the
    Name (100 characters) and Description (1000 characters) properties.

---
+++ src/api/modules/Catalog/Catalog.Infrastructure/Persistence/Configurations/BrandConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using Finbuckle.MultiTenant;
+using FSH.Starter.WebApi.Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FSH.Starter.WebApi.Catalog.Infrastructure.Persistence.Configurations;
+internal sealed class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.IsMultiTenant();
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(100);
+        builder.Property(x => x.Description).HasMaxLength(1000);
+    }
+}
