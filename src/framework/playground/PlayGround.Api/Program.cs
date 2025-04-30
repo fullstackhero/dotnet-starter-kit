4422a5f08f851015b32d49d4c04d463e859db901
Mukesh Murugan <iammukeshm@gmail.com>
2025-04-30 21:48:15 +0530

    scalar

--- src/framework/playground/PlayGround.Api/Program.cs
+++ src/framework/playground/PlayGround.Api/Program.cs
@@ -6,6 +6,7 @@ using FSH.Modules.Common.Infrastructure;
 using FSH.Modules.Identity;
 using FSH.Modules.Tenant;
 using FSH.PlayGround.Api.Extensions;
+using Scalar.AspNetCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +30,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    string[] versions = ["v1", "v2"];
+    app.MapScalarApiReference(options => options.AddDocuments(versions));
 }
 
 
